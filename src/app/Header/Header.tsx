40b490248142d2686123443f7252b93aff26ce61
TheDev-Silva <silvadeveloper2024@gmail.com>
2025-01-28 23:06:35 -0300

    feat: aperfeiçoando

--- src/app/Header/Header.tsx
+++ src/app/Header/Header.tsx
@@ -5,10 +5,13 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 
+
+
 export default function Header() {
 
    const [isScrolled, setIsScrolled] = useState(false)
-   const [logoText, setLogoText] = useState('_The Silva')
+   const [logoText, setLogoText] = useState(false)
+   
 
    useEffect(() => {
 
@@ -27,9 +30,9 @@ export default function Header() {
    useEffect(() => {
       const handleResize = () => {
          if (window.innerWidth <= 768) {
-            setLogoText('</ The SD');
+            setLogoText(true);
          } else {
-            setLogoText(logoText);
+            setLogoText(false);
          }
       };
 
@@ -48,7 +51,7 @@ export default function Header() {
 
 
    return (
-      <div className={`fixed w-full z-40 p-10 transition-all duration-500 ${isScrolled ? 'h-[130px] backdrop-blur-sm' : 'h-[120px] bg-[#6716cf99]'
+      <div className={`fixed w-full z-40 p-10 transition-all duration-500 ${isScrolled ? 'h-[130px] backdrop-blur-sm' : 'h-[120px] bg-[#6716cf]'
          }`}>
          {/* Camada de fundo com desfoque */}
          <div className={`absolute inset-0 bg-gradient-to-tr from-[#6716cf] to-[#000] ${isScrolled ? 'opacity-80' : 'opacity-0'
@@ -61,33 +64,24 @@ export default function Header() {
                   <Link href={'/'}>
                      <div className='flex flex-col justify-items-end'>
                         {/*  <h1 className='flex font-Roseberry md:text-3xl text-xl text-white font-bold text drop-shadow-textsm transition-all duration-300 tracking-widest let'></h1> */}
-                        {window.innerWidth >= 768 ? (
+                        
                            <Image
-                              src={'/logo-completa.png'}
+                              src={!logoText ? '/logo-completa.png' : '/logo-reduzida.png'}
                               alt='logo'
                               width={0}
                               height={0}
                               quality={90}
                               sizes={'100vw'}
-                              className='w-[130px] drop-shadow-textlg'
+                              className={!logoText ? 'w-[130px] drop-shadow-textlg': 'w-[50px] drop-shadow-textlg'}
                            />
-                        ) : <Image
-                           src={'/logo-reduzida.png'}
-                           alt='logo'
-                           width={0}
-                           height={0}
-                           quality={90}
-                           sizes={'100vw'}
-                           className='w-[130px] drop-shadow-textlg'
-                        />}
-                        {/* <h1 className="font-GreenKing text-2xl text-white font-bold text drop-shadow-textsm text-right m-[-6px] mr-[3px] transition-all duration-300">{logoText}</h1> */}
+
                      </div>
                   </Link>
 
                </div>
 
             </div>
-            {window.innerWidth >= 768 ? (
+            {!logoText ? (
                <Link href="/Contacts" className='flex items-center gap-2 justify-center'>
 
                   <BiCommentDots size={24} color="#fff" />
