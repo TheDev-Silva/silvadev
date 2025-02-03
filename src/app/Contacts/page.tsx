da995309b29d7017923f5c86200dac6a649ad73b
TheDev-Silva <silvadeveloper2024@gmail.com>
2025-02-03 05:58:23 -0300

    feat: add Header

--- src/app/Contacts/page.tsx
+++ src/app/Contacts/page.tsx
@@ -7,6 +7,7 @@ import React, { useState } from 'react';
 import Link from 'next/link';
 import { AiFillInstagram, AiOutlineInstagram } from "react-icons/ai";
 import { relative } from 'path';
+import Header from '../Header/Header';
 
 export default function Contact() {
 
@@ -35,7 +36,7 @@ export default function Contact() {
    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
       const { name, value } = e.target;
       const formattedValue = name === 'phone' ? formatPhone(value) : value;
-      
+
       setFormData((prev) => ({ ...prev, [name]: formattedValue }));
    };
 
@@ -43,10 +44,10 @@ export default function Contact() {
       e.preventDefault();
       setIsLoading(true)
 
-      
+
 
       try {
-         const response = await fetch('http://localhost:3333/clients', {
+         const response = await fetch('http://localhost:3333/create-client', {
             method: 'POST',
             headers: {
                'Content-Type': 'application/json',
@@ -77,125 +78,129 @@ export default function Contact() {
 
 
    return (
-      <div className='relative md:flex w-full bg-black items-center md:justify-between justify-center md:pt-[150px] pt-[200px] py-20 px-10 md:px-40 md:h-[100vh] min-h-[100vh]'>
-         {/* Círculo decorativo ao fundo */}
+      <div>
+         <Header />
 
-         <div
-            className='absolute -inset-0 md:w-[200px] md:h-[200px] h-[100px] w-[100px] left-9 top-[30px] rounded-full bg-gradient-to-br to-[#6716cf] from-[#000] transition-all duration-300 blur-md'
-         //style={{ top: '10%', right: '0%', transform: 'translate(-50%, -50%)' }} // Centraliza o círculo atrás do conteúdo
-         >
-         </div>
-         <div
-            className='absolute w-[50px] h-[50px] right-5 bottom-[20px] rounded-full bg-gradient-to-br to-[#6716cf] from-[#000] opacity-90 transition-all duration-300 blur-md'
-         //style={{ top: '10%', right: '0%', transform: 'translate(-50%, -50%)' }} // Centraliza o círculo atrás do conteúdo
-         >
+         <div className='relative md:flex w-full bg-black items-center md:justify-between justify-center md:pt-[150px] pt-[200px] py-20 px-10 md:px-40 md:h-[100vh] min-h-[100vh]'>
+            {/* Círculo decorativo ao fundo */}
 
-         </div>
-         <div
-            className='absolute md:w-[50px] md:h-[50px] w-[40px] h-[40px] left-5  rounded-full bg-gradient-to-br to-[#6716cf] from-[#000] bottom-[100px] transition-all duration-300 blur-md'
-         //style={{ top: '10%', right: '0%', transform: 'translate(-50%, -50%)' }} // Centraliza o círculo atrás do conteúdo
-         >
+            <div
+               className='absolute -inset-0 md:w-[200px] md:h-[200px] h-[100px] w-[100px] left-9 top-[30px] rounded-full bg-gradient-to-br to-[#6716cf] from-[#000] transition-all duration-300 blur-md'
+            //style={{ top: '10%', right: '0%', transform: 'translate(-50%, -50%)' }} // Centraliza o círculo atrás do conteúdo
+            >
+            </div>
+            <div
+               className='absolute w-[50px] h-[50px] right-5 bottom-[20px] rounded-full bg-gradient-to-br to-[#6716cf] from-[#000] opacity-90 transition-all duration-300 blur-md'
+            //style={{ top: '10%', right: '0%', transform: 'translate(-50%, -50%)' }} // Centraliza o círculo atrás do conteúdo
+            >
 
-         </div>
-         <div
-            className='absolute md:w-[150px] md:h-[150px] max-h-md w-[80px] h-[80px] rounded-full bg-gradient-to-tr to-[#6716cf] from-[#000] right-10 top-52 bottom-[100px] transition-all duration-300 blur-md'
-         //style={{ top: '10%', right: '0%', transform: 'translate(-50%, -50%)' }} // Centraliza o círculo atrás do conteúdo
-         ></div>
-         <div className='relative z-10 mb-32 md:mb-0'>
-            <h1 className='text-white font-mono text-4xl mb-4'>Contato</h1>
-            <p className='text-white font-mono mb-2'>Entre em contato conosco!</p>
-            <p className='text-white font-mono mb-2'>Telefone: (85) 99775-9542</p>
-            <p className='text-white font-mono mb-2'>E-mail: silvadeveloper2024@gmail.com</p>
-            <div className='flex flex-col'>
-               <p className='text-white font-mono mb-2'>redes socias </p>
-               <div className='flex gap-10'>
-                  <Link href={'https://www.instagram.com/silva.developer/'}>
-                     <AiOutlineInstagram size={30} color='#ffff' />
-                  </Link>
-                  <Link href={'https://github.com/TheDev-Silva/'}>
-                     <AiOutlineGithub size={30} color='#ffff' />
-                  </Link>
-                  <Link href={'https://www.linkedin.com/in/germano-silva-909372248/'}>
-                     <AiOutlineLinkedin size={30} color='#ffff' />
-                  </Link>
+            </div>
+            <div
+               className='absolute md:w-[50px] md:h-[50px] w-[40px] h-[40px] left-5  rounded-full bg-gradient-to-br to-[#6716cf] from-[#000] bottom-[100px] transition-all duration-300 blur-md'
+            //style={{ top: '10%', right: '0%', transform: 'translate(-50%, -50%)' }} // Centraliza o círculo atrás do conteúdo
+            >
+
+            </div>
+            <div
+               className='absolute md:w-[150px] md:h-[150px] max-h-md w-[80px] h-[80px] rounded-full bg-gradient-to-tr to-[#6716cf] from-[#000] right-10 top-52 bottom-[100px] transition-all duration-300 blur-md'
+            //style={{ top: '10%', right: '0%', transform: 'translate(-50%, -50%)' }} // Centraliza o círculo atrás do conteúdo
+            ></div>
+            <div className='relative z-10 mb-32 md:mb-0'>
+               <h1 className='text-white font-mono text-4xl mb-4'>Contato</h1>
+               <p className='text-white font-mono mb-2'>Entre em contato conosco!</p>
+               <p className='text-white font-mono mb-2'>Telefone: (85) 99775-9542</p>
+               <p className='text-white font-mono mb-2'>E-mail: silvadeveloper2024@gmail.com</p>
+               <div className='flex flex-col'>
+                  <p className='text-white font-mono mb-2'>redes socias </p>
+                  <div className='flex gap-10'>
+                     <Link href={'https://www.instagram.com/silva.developer/'}>
+                        <AiOutlineInstagram size={30} color='#ffff' />
+                     </Link>
+                     <Link href={'https://github.com/TheDev-Silva/'}>
+                        <AiOutlineGithub size={30} color='#ffff' />
+                     </Link>
+                     <Link href={'https://www.linkedin.com/in/germano-silva-909372248/'}>
+                        <AiOutlineLinkedin size={30} color='#ffff' />
+                     </Link>
+                  </div>
+
                </div>
 
             </div>
+            {/* Conteúdo principal */}
+            <div
+               style={{
+                  flexDirection: 'column',
+                  borderColor: '#6716cf',
+                  borderBottomLeftRadius: 20,
+                  backgroundColor: '#6716cf29',
+                  borderRadius: 30,
+                  marginTop: 40,
+                  position: 'relative',
+                  justifyContent: 'center',
+                  alignItems: 'center'
+               }}
+               className="border-neon shadow-neon"
+            >
+               <div className='relative bg-[#6716cf29] p-4 justify-center items-center' style={{ borderRadius: 30 }}>
+                  <p className='text-white font-mono m-4 mb-[-20px] text-center'>Vamos criar um projeto incrivel juntos?</p>
+                  <p className='text-white font-mono m-4 '>Mande sua ideia.</p>
 
-         </div>
-         {/* Conteúdo principal */}
-         <div
-            style={{
-               flexDirection: 'column',
-               borderColor: '#6716cf',
-               borderBottomLeftRadius: 20,
-               backgroundColor: '#6716cf29',
-               borderRadius: 30,
-               marginTop: 40,
-               position: 'relative',
-               justifyContent: 'center',
-               alignItems: 'center'
-            }}
-            className="border-neon shadow-neon"
-         >
-            <div className='relative bg-[#6716cf29] p-4 justify-center items-center' style={{ borderRadius: 30 }}>
-               <p className='text-white font-mono m-4 mb-[-20px] text-center'>Vamos criar um projeto incrivel juntos?</p>
-               <p className='text-white font-mono m-4 '>Mande sua ideia.</p>
+                  <form onSubmit={handleSubmit} className='w-fill flex flex-col gap-5 p-4'>
+                     <div className='flex flex-col relative z-10'>
+                        {/* <label className='absolute left-2 top-2 opacity-45 hover:top-[-4px]'>Nome</label> */}
+                        <input
+                           type="name"
+                           name='name'
+                           autoComplete={'name'}
+                           placeholder="Nome completo"
+                           value={formData.name}
+                           onChange={handleChange}
+                           required
+                           className='w-[400px], p-2 rounded-sm'
 
-               <form onSubmit={handleSubmit} className='w-fill flex flex-col gap-5 p-4'>
-                  <div className='flex flex-col relative z-10'>
-                     {/* <label className='absolute left-2 top-2 opacity-45 hover:top-[-4px]'>Nome</label> */}
+                        />
+                     </div>
+
                      <input
-                        type="name"
-                        name='name'
-                        autoComplete={'name'}
-                        placeholder="Nome completo"
-                        value={formData.name}
+                        type="email"
+                        name='email'
+                        autoComplete={'email'}
+                        placeholder="E-mail"
+                        value={formData.email}
                         onChange={handleChange}
                         required
-                        className='w-[400px], p-2 rounded-sm'
-
+                        className='w-[400px], p-2 rounded-sm '
                      />
-                  </div>
+                     <input
+                        type="tel"
+                        autoComplete={'tel'}
+                        name='phone'
+                        placeholder="Telefone"
+                        value={formData.phone}
+                        onChange={handleChange}
+                        required
+                        className='w-[400px], p-2 rounded-sm '
+                     />
+                     <textarea
+                        name='message'
+                        placeholder="Sua mensagem"
+                        value={formData.message}
+                        onChange={handleChange}
+                        required
+                        className='h-200 p-2'
+                     >
 
-                  <input
-                     type="email"
-                     name='email'
-                     autoComplete={'email'}
-                     placeholder="E-mail"
-                     value={formData.email}
-                     onChange={handleChange}
-                     required
-                     className='w-[400px], p-2 rounded-sm '
-                  />
-                  <input
-                     type="tel"
-                     autoComplete={'tel'}
-                     name='phone'
-                     placeholder="Telefone"
-                     value={formData.phone}
-                     onChange={handleChange}
-                     required
-                     className='w-[400px], p-2 rounded-sm '
-                  />
-                  <textarea
-                     name='message'
-                     placeholder="Sua mensagem"
-                     value={formData.message}
-                     onChange={handleChange}
-                     required
-                     className='h-200 p-2'
-                  >
-
-                  </textarea>
-                  {isLoading ? <button type="submit" className='flex w-full text-white hover:bg-[#6716cf] bg-[#6716cf89] transition-all duration-300 p-3 rounded-sm text-center items-center justify-center gap-3'>
-                     <AiOutlineLoading className='animate-spin ease-linear spin-in-45' size={24} /> ENVIANDO SUA IDEIA...
-                  </button> : <button type="submit" className='flex w-full text-white hover:bg-[#6716cf] bg-[#6716cf89] transition-all duration-300 p-3 rounded-sm text-center items-center justify-center text-sm font-thin'>ENVIAR IDEIA</button>}
-               </form>
-               {responseMessage && <p className='text-white'> {responseMessage}</p>}
+                     </textarea>
+                     {isLoading ? <button type="submit" className='flex w-full text-white hover:bg-[#6716cf] bg-[#6716cf89] transition-all duration-300 p-3 rounded-sm text-center items-center justify-center gap-3'>
+                        <AiOutlineLoading className='animate-spin ease-linear spin-in-45' size={24} /> ENVIANDO SUA IDEIA...
+                     </button> : <button type="submit" className='flex w-full text-white hover:bg-[#6716cf] bg-[#6716cf89] transition-all duration-300 p-3 rounded-sm text-center items-center justify-center text-sm font-thin'>ENVIAR IDEIA</button>}
+                  </form>
+                  {responseMessage && <p className='text-white'> {responseMessage}</p>}
+               </div>
             </div>
-         </div>
 
+         </div>
       </div>
    );
 }
