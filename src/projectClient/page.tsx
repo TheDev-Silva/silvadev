9e2109767756f734aebc43518925edd73a8d5bd5
TheDev-Silva <silvadeveloper2024@gmail.com>
2025-02-03 06:09:48 -0300

    feat: nova página sendo construida...

--- src/projectClient/page.tsx
+++ src/projectClient/page.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function ProjectClient() {
   return (
     <div
-      className='h-full bg-slate-400 '
-      style={{height: '100%', color: '#fff'}}
-    >ProjectClient</div>
+    className='flex w-full h-[400px] flex-col md:w-full md:h-[100vw] items-center justify-between md:flex relative bg-slate-950' 
+    /* style={{height: 400}} */
+    ><h1 className='text-slate-50'>Nossos serviços</h1></div>
   )
 }
