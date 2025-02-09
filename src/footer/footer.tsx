57cbc420657d0ce868b458f020f9f542e9c41a86
TheDev-Silva <silvadeveloper2024@gmail.com>
2025-02-09 16:13:00 -0300

    feat: centralizando a responsividade.

--- src/footer/footer.tsx
+++ src/footer/footer.tsx
@@ -3,15 +3,15 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='md:flex flex-coll w-full md:justify-center justify-between items-center gap-3 pt-10 mt-10 text-center' style={{ borderTopWidth: 1, borderTopColor: '#c1c1c1'}}>
-            <div className='flex gap-3 md:justify-center justify-start items-center font-mono'>
+        <div className='md:flex flex-coll w-full md:justify-center justify-between items-center gap-3 pt-10 mt-10 text-center' style={{ borderTopWidth: 1, borderTopColor: '#c1c1c1' }}>
+            <div className='flex gap-3 justify-center md:justify-start items-center font-mono'>
                 <h1 className='text-white '>silvadev</h1>
                 <Copyright color='#fff' />
                 <h1 className='text-white '>Todos os direitos recervados.</h1>
             </div>
-            
-                <h1 className='text-white font-mono'>suporte urgente chama no zap!</h1>
-            
+
+            <h1 className='text-white font-mono'>suporte urgente chama no zap!</h1>
+
         </div>
     )
 }
